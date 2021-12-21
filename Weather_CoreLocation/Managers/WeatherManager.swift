688ae1b451eafe13fc72fb3e25338e421cfe2281
Stanley Pan <imoment.developer@gmail.com>
2021-12-21 15:27:21 -0500

    Added WeatherManager class

---
+++ Weather_CoreLocation/Managers/WeatherManager.swift
@@ -0,0 +1,60 @@
+//
+//  WeatherManager.swift
+//  Weather_CoreLocation
+//
+//  Created by Stanley Pan on 2021/12/21.
+//
+
+import Foundation
+import CoreLocation
+
+class WeatherManager {
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws {
+        guard let url = URL(string: "api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\("4850390e6b5efd45bdd3c741b603224b")&units=metric") else { fatalError("Missing URL")}
+        
+        let urlRequest = URLRequest(url: url)
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error fetching weather data")}
+    }
+}
+
+struct ResponseBody: Decodable {
+    var coord: CoordinatesResponse
+    var weather: [WeatherResponse]
+    var main: MainResponse
+    var name: String
+    var wind: WindResponse
+
+    struct CoordinatesResponse: Decodable {
+        var lon: Double
+        var lat: Double
+    }
+
+    struct WeatherResponse: Decodable {
+        var id: Double
+        var main: String
+        var description: String
+        var icon: String
+    }
+
+    struct MainResponse: Decodable {
+        var temp: Double
+        var feels_like: Double
+        var temp_min: Double
+        var temp_max: Double
+        var pressure: Double
+        var humidity: Double
+    }
+    
+    struct WindResponse: Decodable {
+        var speed: Double
+        var deg: Double
+    }
+}
+
+extension ResponseBody.MainResponse {
+    var feelsLike: Double { return feels_like }
+    var tempMin: Double { return temp_min }
+    var tempMax: Double { return temp_max }
+}
