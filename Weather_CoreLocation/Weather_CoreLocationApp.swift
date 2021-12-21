fe1cd0f05dd0444069e7fa427949f1a3500174e9
Stanley Pan <imoment.developer@gmail.com>
2021-12-21 11:22:30 -0500

    Initial Commit

---
+++ Weather_CoreLocation/Weather_CoreLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_CoreLocationApp.swift
+//  Weather_CoreLocation
+//
+//  Created by Stanley Pan on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Weather_CoreLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
