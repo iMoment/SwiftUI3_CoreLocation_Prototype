713a20a436d1092fa7ea25e807512f91ca45d3ca
Stanley Pan <imoment.developer@gmail.com>
2021-12-21 15:36:20 -0500

    Fully implemented getting data from CLLocation coordinates

--- Weather_CoreLocation/Managers/WeatherManager.swift
+++ Weather_CoreLocation/Managers/WeatherManager.swift
@@ -9,13 +9,17 @@ import Foundation
 import CoreLocation
 
 class WeatherManager {
-    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws {
-        guard let url = URL(string: "api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\("4850390e6b5efd45bdd3c741b603224b")&units=metric") else { fatalError("Missing URL")}
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\("4850390e6b5efd45bdd3c741b603224b")&units=metric") else { fatalError("Missing URL")}
         
         let urlRequest = URLRequest(url: url)
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
         
         guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error fetching weather data")}
+        
+        let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
+        
+        return decodedData
     }
 }
 
