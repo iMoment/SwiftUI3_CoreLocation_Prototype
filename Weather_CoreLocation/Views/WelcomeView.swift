098c50cbf1b910a670f1560dbcba25c48a681a12
Stanley Pan <imoment.developer@gmail.com>
2021-12-21 15:13:15 -0500

    Add LoadingView, WelcomeView, and conditional rendering to ContentView

---
+++ Weather_CoreLocation/Views/WelcomeView.swift
@@ -0,0 +1,41 @@
+//
+//  WelcomeView.swift
+//  Weather_CoreLocation
+//
+//  Created by Stanley Pan on 2021/12/21.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    
+    var body: some View {
+        VStack {
+            VStack(spacing: 20) {
+                Text("Welcome to the CoreLocation Prototype")
+                    .bold().font(.title)
+                
+                Text("Please share your current location to get the weather in your area.")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
